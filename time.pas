4fb44391067e0995ec7b33f3751d0f99e0ff9734
Лилия Кошеляева <lilya.koshelyaeva@mail.ru>
2023-09-30 08:34:11 +0300

    Ваше сообщение

---
+++ time.pas
@@ -0,0 +1,7 @@
+﻿begin
+  var x := ReadInteger('Введите значение x:');
+  if (x mod 4 = 0) and ((x mod 100 <> 0) or (x mod 400 = 0)) then
+    print('Високосный год')
+  else
+    print('Невисокосный год');
+end.
